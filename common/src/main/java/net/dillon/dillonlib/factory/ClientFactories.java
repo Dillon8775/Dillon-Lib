6df35a607c52fce67c0b8dd5ac5dde0084ceaad1
Dillon <dilloneystrickland12@gmail.com>
2026-07-23 16:19:36 -0400

    real official release - final changes
    # Conflicts:
    #	common/src/main/java/net/dillon/dillonlib/core/DillonLibEvents.java
    #	common/src/main/java/net/dillon/dillonlib/factory/Factories.java

--- common/src/main/java/net/dillon/dillonlib/factory/ClientFactories.java
+++ common/src/main/java/net/dillon/dillonlib/factory/ClientFactories.java
@@ -3,7 +3,7 @@ package net.dillon.dillonlib.factory;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.dillon.dillonlib.annotation.Dill;
 import net.dillon.dillonlib.annotation.DillType;
-import net.dillon.dillonlib.platform.PlatformGetter;
+import net.dillon.dillonlib.core.DillonLibMain;
 import net.dillon.dillonlib.platform.client.ClientPlatformGetter;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.model.geom.ModelLayerLocation;
@@ -30,7 +30,7 @@ public class ClientFactories {
     public static KeyMapping registerKeyMapping(String key, InputConstants.Type type, KeyMapping.Category category, int code, Runnable runnable) {
         KeyMapping keyBind = ClientPlatformGetter.getDillonLibClientPlatform().createKeyMapping(key, type, category, code);
         NON_KUMA_KEY_MAPPING_FACTORIES.put(keyBind, runnable);
-        PlatformGetter.getDillonLibPlatform().logger().info("Registered key mapping {} with keycode {}", key, code);
+        DillonLibMain.LOGGER.info("Registered key mapping {} with keycode {}", key, code);
         return keyBind;
     }
 
