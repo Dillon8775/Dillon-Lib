e4e58e45cf1eeaa3009ff2b4bc878706a744e1ac
Dillon <dilloneystrickland12@gmail.com>
2026-07-23 23:29:22 -0400

    real official release - final changes

--- common/src/main/java/net/dillon/dillonlib/factory/Factories.java
+++ common/src/main/java/net/dillon/dillonlib/factory/Factories.java
@@ -98,7 +98,7 @@ public class Factories {
     }
 
     /**
-     * Creates a simple item group. Should be called in your mod's common initialization stage or in {@link net.dillon.dillonlib.platform.ModPlatform}. This will register your item group on all platforms automatically (fabric x (neo)forge).
+     * Creates a simple item group with a custom supplier for your items. Should be called in your mod's common initialization stage. This will register your item group on all platforms automatically (fabric x (neo)forge).
      * @param id the {@link Identifier} for your item group (used to create the translation key)
      * @param icon the icon for your item group
      * @param entries the list of items to be in your item group
@@ -106,6 +106,20 @@ public class Factories {
     public static void registerSimpleItemGroupFactory(Identifier id, ItemLike icon, Supplier<List<ItemStack>> entries) {
         CommonPlatformGetter.get().registerItemGroup(new SimpleItemGroupFactory(id, icon, entries));
         DillonLibMain.LOGGER.debug("Registered item group factory {}", id);
+    }
+
+    /**
+     * Creates a simple item group with basic items.
+     * @param id the {@link Identifier} for your item group (used to create the translation key)
+     * @param icon the icon for your item group
+     * @param entries the list of {@link ItemLike}s to be in your item group
+     */
+    public static void registerSimpleItemGroupFactory(Identifier id, ItemLike icon, List<ItemLike> entries) {
+        registerSimpleItemGroupFactory(id, icon, () ->
+                entries.stream()
+                        .map(itemLike -> itemLike.asItem().getDefaultInstance())
+                        .toList()
+        );
     }
 
     /**
