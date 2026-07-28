a1ee92fc31e527624f21d2e2ebe4275c83692b24
Dillon <dilloneystrickland12@gmail.com>
2026-07-28 17:19:22 -0400

    real official release - final changes

---
+++ common/src/main/java/net/dillon/dillonlib/util/CommonSprites.java
@@ -0,0 +1,12 @@
+package net.dillon.dillonlib.util;
+
+import net.dillon.dillonlib.core.DillonLibMain;
+import net.minecraft.resources.Identifier;
+
+/**
+ * Common sprites used throughout Dillon's mods.
+ */
+public class CommonSprites {
+    public static final Identifier BOOK = Identifier.fromNamespaceAndPath(DillonLibMain.MOD_ID, "container/slot/book");
+    public static final Identifier ENCHANTED_BOOK = Identifier.fromNamespaceAndPath(DillonLibMain.MOD_ID, "container/slot/enchanted_book");
+}
