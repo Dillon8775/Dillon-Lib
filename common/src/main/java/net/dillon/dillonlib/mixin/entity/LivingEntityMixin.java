8f6e0efc5db430f7d2d6a868e6f6023fbca5878f
Dillon <dilloneystrickland12@gmail.com>
2026-07-31 14:37:16 -0400

    improve totem factory

--- common/src/main/java/net/dillon/dillonlib/mixin/entity/LivingEntityMixin.java
+++ common/src/main/java/net/dillon/dillonlib/mixin/entity/LivingEntityMixin.java
@@ -27,8 +27,8 @@ public abstract class LivingEntityMixin {
     /**
      * Makes {@link TotemFactory}s work correctly.
      */
-    @Inject(method = "checkTotemDeathProtection", at = @At(value = "RETURN", ordinal = 1), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
-    private void checkTotemDeathProtectionFactory(DamageSource source, CallbackInfoReturnable<Boolean> cir, ItemStack nullStack, DeathProtection protection) {
+    @Inject(method = "checkTotemDeathProtection", at = @At(value = "RETURN", ordinal = 1), cancellable = true)
+    private void checkTotemDeathProtectionFactory(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity self = (LivingEntity) (Object)this;
         for (InteractionHand hand : InteractionHand.values()) {
             ItemStack totem = this.getItemInHand(hand);
