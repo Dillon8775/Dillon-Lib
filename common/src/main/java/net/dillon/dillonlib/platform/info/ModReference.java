4bf87154a9a0052ca11b6c37c9c0c32283d7a492
Dillon <dilloneystrickland12@gmail.com>
2026-09-19 14:21:05 -0400

    protect constructors

--- common/src/main/java/net/dillon/dillonlib/platform/info/ModReference.java
+++ common/src/main/java/net/dillon/dillonlib/platform/info/ModReference.java
@@ -3,11 +3,13 @@ package net.dillon.dillonlib.platform.info;
 /**
  * Used to create {@code mod id references}.
  * @since 1.0
- * @param modId the mod identifier to reference when checking if a certain mod is loaded.
  */
 public class ModReference {
     private final String modId;
 
+    /**
+     * @param modId the mod identifier to reference when checking if a certain mod is loaded.
+     */
     private ModReference(final String modId) {
         this.modId = modId;
     }
